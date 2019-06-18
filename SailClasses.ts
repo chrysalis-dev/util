31f758ee58e180910119bddc9b2b5d1a09465185
Mihail-Emanuel Ghinea <32837791+thMike@users.noreply.github.com>
2019-06-18 11:29:50 +0100

    Add TypeScript implementation of the classes

---
+++ SailClasses.ts
@@ -0,0 +1,50 @@
+class Competition
+{
+    Name: string;
+    StartDate: Date;
+    EndDate: Date;
+    Races: Array<Race> = new Array<Race>();
+
+    constructor(name: string, start: Date, end: Date)
+    {
+        this.Name = name;
+        this.StartDate = start;
+        this.EndDate = end;
+    }
+}
+
+class Race
+{
+    RaceID: number;
+    RaceDate: Date;
+    Results: Array<RaceResult> = new Array<RaceResult>();
+
+    constructor(id: number, date: Date)
+    {
+        this.RaceID = id;
+        this.RaceDate = date;
+    }
+}
+
+class RaceResult
+{
+    BoatID: number;
+    Rank: number;
+    ElapsedTime: number;
+    Laps: number;
+    CorrectedTime: number;
+    Points: number;
+    SpecialMentions: string;
+
+    constructor(id: number, rank: number, elapsed: number, laps: number, corrected: number, points: number, mentions: string)
+    {
+        this.BoatID = id;
+        this.Rank = rank;
+        this.ElapsedTime = elapsed;
+        this.Laps = laps;
+        this.CorrectedTime = corrected;
+        this.Points = points;
+        this.SpecialMentions = mentions;
+    }
+}
+
